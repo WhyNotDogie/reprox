8f8406f4dcb11019bb8d9443788ee1a2c9f6602c
Kees Johnson <keesjejohnson@gmail.com>
2023-05-13 09:56:21 +0200

    moreeeeeeee codeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- justfile
+++ justfile
@@ -1,2 +1,4 @@
-run:
-    cargo run --bin reprox
+default: run
+
+run a="" b="" c="" d="" e="" f="" g="" h="" i="" j="" k="" l="" m="" n="" o="" p="":
+    cargo run --bin reprox {{a}} {{b}} {{c}} {{d}} {{e}} {{f}} {{g}} {{h}} {{i}} {{j}} {{k}} {{l}} {{m}} {{n}} {{o}} {{p}}
--- reprox/src/main.rs
+++ reprox/src/main.rs
@@ -1,7 +1,36 @@
 mod server;
-mod arg;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(subcommand)]
+    commands: Command
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Start the reprox server.
+    Start {
+        #[clap(long, short='l')]
+        log_mode: bool,
+        /// The port that the TCP server will be hosted on.
+        #[clap(long, short='p')]
+        input_port: u32
+    }
+}
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    log!("Starting server on port: {}", 1024);
-    server::run(1024)
+    let args = Args::parse();
+    match args.commands {
+        Command::Start {input_port, log_mode} => {
+            if log_mode {
+                std::env::set_var("REPROX_LOG_MODE", "true");
+            }
+            log!("Starting server on port: {}", input_port);
+            server::run(input_port)?;
+        }
+    }
+    Ok(())
 }
